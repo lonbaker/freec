62fd7d08ccfd44b18f92a3ff063b664f86078347
Jan Kubr <jan.kubr@gmail.com>
2009-05-14 23:27:55 +0200

    Added miliseconds to timestamps in log messages

--- lib/freec_logger.rb
+++ lib/freec_logger.rb
@@ -2,7 +2,7 @@ require 'logger'
 
 class FreecLogger < Logger
     def format_message(severity, timestamp, progname, msg)  #:nodoc:
-      "#{timestamp.strftime("%Y-%m-%d %H:%M:%S")} #{severity} #{msg}\n"
+      "#{timestamp.strftime("%Y-%m-%d %H:%M:%S")} #{(timestamp.usec / 1000.0).round} #{severity} #{msg}\n"
     end
     
     # Prints the message with twenty #'s before and after it.
